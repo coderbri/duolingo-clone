61cd49b724590092bb7c5093e52b62e63c6d3a84
coderBri <coderbri23@gmail.com>
2024-05-03 01:45:12 -0400

    Created a Marketing Skeleton along with Header and Footer ccmponents.

--- lingo/app/(marketing)/page.tsx
+++ lingo/app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return(
     <div>
-      <p>Hello Lingo</p>
+      <p>This is a marketing page</p>
       <Button size="lg">Click me</Button>
     </div>
   )
